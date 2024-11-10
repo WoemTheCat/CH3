42eea12e8ce20f9dccf22a42b767710f7afcfc54
WoemTheCat <t111820031@ntut.org.tw>
2024-11-10 09:50:03 +0800

    Sample3_3.java

--- Sample3_3.java
+++ Sample3_3.java
@@ -1,17 +1,22 @@
-public class Sample3_2 {
+public class Sample3_3 {
     public static void main(String[] args) {
-        Car car1;
+        Car car1, car2;
         car1 = new Car();
+        car2 = new Car();
 
         car1.num = 1234;
         car1.gas = 20.5;
+        car2.num = 2345;
+        car2.gas = 30.5;
 
         System.out.println("車號是" + car1.num);
         System.out.println("汽油輛是" + car1.gas);
+        System.out.println("車號是" + car2.num);
+        System.out.println("汽油輛是" + car2.gas);
     }
 }
 
-class Car {
+class car {
     int num;
     double gas;
 }
